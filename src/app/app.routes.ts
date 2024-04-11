ef00840d13d70e67541103e9e6fda673dec1c0ef
wanderlee <wanderleedev@gmail.com>
2024-04-11 11:45:26 -0500

    feat: Added Paths and Creating Components and Pages Folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full',
+  // },
+  // {
+  //   path: 'home',
+  //   component: HomeComponent,
+  //   title: 'Preguntonic | Home',
+  // },
+  // {
+  //   path: 'waiting-room',
+  //   loadComponent: () =>
+  //     import('./Pages/waiting-room/waiting-room.component').then(
+  //       m => m.WaitingRoomComponent
+  //     ),
+  //   title: 'Preguntonic | Waiting room',
+  // },
+  // {
+  //   path: 'game-room',
+  //   loadComponent: () =>
+  //     import('./Pages/game-room/game-room.component').then(
+  //       m => m.GameRoomComponent
+  //     ),
+  //   title: 'Preguntonic | Game room',
+  // },
+  // {
+  //   path: 'results-room',
+  //   loadComponent: () =>
+  //     import('./Pages/results-room/results-room.component').then(
+  //       m => m.ResultsRoomComponent
+  //     ),
+  //   title: 'Preguntonic | Results room',
+  // },
+  // {
+  //   path: '**',
+  //   loadComponent: () =>
+  //     import('./Pages/not-found/not-found.component').then(
+  //       m => m.NotFoundComponent
+  //     ),
+  //   title: 'Preguntonic | Not found 404',
+  // },
+];
