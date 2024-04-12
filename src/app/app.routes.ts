3c184a8e5e1c55e459fdfdd32c1d70b3789c5df5
NicolasLLans <nicolaslautaro22@gmail.com>
2024-04-12 18:34:17 -0300

    feat:create homepage and custom button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full',
-  // },
-  // {
-  //   path: 'home',
-  //   component: HomeComponent,
-  //   title: 'Preguntonic | Home',
-  // },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Preguntonic | Home',
+  },
   // {
   //   path: 'waiting-room',
   //   loadComponent: () =>
