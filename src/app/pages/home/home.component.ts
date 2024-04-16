e328b80d221542636dc0a53adf54c9aa4520fb75
NicolasLLans <nicolaslautaro22@gmail.com>
2024-04-16 15:20:23 -0300

    fix:Standardization of folders to kebab-case. modifed home - add avatar components and select name and avatar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,51 @@
-import { Component } from '@angular/core';
-import { LogoTitleComponent } from "../../shared/UI/logoTitle/logoTitle.component";
-import { AvatarImageComponent } from '../../components/avatarImage/avatar-image.component';
-import { CustomButtonComponent } from '../../components/customButton/custom-button.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LogoTitleComponent } from "../../shared/components/logo-title/logo-title.component";
+import { AvatarImageComponent } from '../../components/avatar-image/avatar-image.component';
+import { CustomButtonComponent } from '../../components/custom-button/custom-button.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
+    changeDetection:ChangeDetectionStrategy.OnPush,
     styleUrl: './home.component.css',
     imports: [CustomButtonComponent, LogoTitleComponent, AvatarImageComponent]
 })
 export class HomeComponent {
     avatarImages: string[] = [
-        'https://via.placeholder.com/300',
-        'https://via.placeholder.com/300/ff0000',
-        'https://via.placeholder.com/300/0000ff'
+        '../../../assets/avatar-1.webp',
+        '../../../assets/avatar-2.webp',
+        '../../../assets/avatar-3.webp',
+        '../../../assets/avatar-4.webp'
       ];
+      selectedAvatar: string = '';
+      playerName: string = '';
+    
+      selectAvatar(avatar: string) {
+        this.selectedAvatar = avatar;
+      }
+    
+      onNameInputChange(event: Event) {
+        // Utiliza la interfaz HTMLInputElement para asegurar que event.target es un input element
+        const inputElement = event.target as HTMLInputElement;
+        this.playerName = inputElement.value;
+      }
+      iniciarPartida() {
+        // Verificar si se ha ingresado un nombre
+        if (this.playerName.trim() !== '') {
+          // Crear el objeto JSON con la información capturada
+          const playerData = {
+            avatar: this.selectedAvatar,
+            playerName: this.playerName
+          };
+      
+          // Imprimir el objeto JSON en consola
+          console.log(playerData);
+        } else {
+          // Mostrar una alerta si no se ha ingresado un nombre
+          alert("Por favor, ingresa un nombre.");
+        }
+      }
+      
+      
 }
