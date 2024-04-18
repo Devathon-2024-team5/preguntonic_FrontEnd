82e65210b72ac0bde73c855e987173b2b57480ce
Alejandro25RA <alejito25raiders@gmail.com>
2024-04-17 22:47:31 -0500

    add properties for use in the component view
    valuesNumberPlayer: List of player select tag values
    valuesNumberQuestion: List of question select tag values
    numberOfPlayersInTheRoom: Number of players that can join the room selected by the user
    numberOfGameQuestions: number of user-selected questions for the questionnaire

--- src/app/pages/room-configuration/room-configuration.component.ts
+++ src/app/pages/room-configuration/room-configuration.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './room-configuration.component.css'
 })
 export class RoomConfigurationComponent {
+  valuesSelectPlayer = [2,3,4,5,6,7,8];
+  valuesSelectQuestion = [5,10,15,20,25,30];
 
+  numberOfPlayersInTheRoom = this.valuesSelectPlayer[0];
+  numberOfGameQuestions = this.valuesSelectQuestion[0];
 }
