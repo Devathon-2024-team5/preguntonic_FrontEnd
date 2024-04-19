b6158e534df936350bd4bd25a79651862c57d1ec
NicolasLLans <nicolaslautaro22@gmail.com>
2024-04-19 17:58:23 -0300

    model: connect anteroom

--- src/app/shared/services/web.socket.api.ts
+++ src/app/shared/services/web.socket.api.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
 import {Stomp} from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
@@ -17,22 +18,24 @@ export class WebSocketAPI {
     _connect(roomId: string, player_name: string, avatar_id: string) {
         this.roomId = roomId;
         console.log("Initialize WebSocket Connection");
-        let ws = new SockJS(this.webSocketEndPoint);
+        const ws = new SockJS(this.webSocketEndPoint);
         this.stompClient = Stomp.over(ws);
+        // eslint-disable-next-line @typescript-eslint/no-this-alias
         const _this = this;
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         _this.stompClient.connect({}, function (frame: any) {
             _this.stompClient.subscribe(_this.topic + roomId, function (wsResponse: any) {
                 _this.onMessageReceived(wsResponse);
             });
             //_this.stompClient.reconnect_delay = 2000;
-            let response = _this.stompClient.send(`/app/rooms.join/${roomId}`, {}, JSON.stringify({'player_name': player_name, 'avatar_id': avatar_id}));
+            const response = _this.stompClient.send(`/app/rooms.join/${roomId}`, {}, JSON.stringify({'player_name': player_name, 'avatar_id': avatar_id}));
             console.log("response stomp send: ", response);
         }, this.errorCallBack);
         // if (this.stompClient.status === 'CONNECTED') {
         //     let response = this.stompClient.send(`/app/rooms.join/${roomId}`, {}, JSON.stringify({'player_name': player_name, 'avatar_id': avatar_id}));
         //     console.log("response stomp send: ", response);
         // }
-    };
+    }
 
     _disconnect() {
         if (this.stompClient !== null) {
