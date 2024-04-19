860ace2cf0b33e2cffd1d4af93ee67c27ade1ea7
wanderlee <wanderleedev@gmail.com>
2024-04-19 09:54:56 -0500

    finished navbar daily

--- src/app/components/navbar-daily/navbar-daily.component.ts
+++ src/app/components/navbar-daily/navbar-daily.component.ts
@@ -1,14 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { routes } from '../../app.routes';
+import { RESULTS_ROUTES } from '../../pages/results-room/results-room.routes'
+import { CapitalizePipe } from '../../shared/pipes/capitalize.pipe';
 
 @Component({
   selector: 'app-navbar-daily',
   standalone: true,
   imports: [
     CommonModule,
+    RouterLink,
+    RouterLinkActive,
+    CapitalizePipe
   ],
   templateUrl: './navbar-daily.component.html',
   styleUrl: './navbar-daily.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarDailyComponent { }
+export class NavbarDailyComponent implements OnInit  {
+  viewPages: (string | undefined)[] = [];
+
+  ngOnInit(): void {
+    const routes1 = routes.map(route => route?.path);
+    const routes2 = RESULTS_ROUTES[0].children?.map(route => route.path) ?? [];
+
+    this.viewPages = [...routes1, ...routes2].filter(r => (r !== 'results-room' && r !== ''));
+    console.log(this.viewPages);
+  }
+}
