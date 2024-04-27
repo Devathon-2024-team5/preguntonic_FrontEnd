2202fb2052c81a238f3b83248d7fe5cfd41b9df4
Wanderlee Max <116982723+WanderleeDev@users.noreply.github.com>
2024-04-27 01:04:01 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarDailyComponent } from './components/navbar-daily/navbar-daily.component';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarDailyComponent, AsyncPipe],
+  imports: [RouterOutlet, NavbarDailyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
