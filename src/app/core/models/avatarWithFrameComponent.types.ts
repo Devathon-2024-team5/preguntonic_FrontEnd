4478618a087d00e1a84d69a6af8557992f09299e
Alejandro25RA <alejito25raiders@gmail.com>
2024-05-02 11:30:39 -0500

    add types of values that are acceptor by Component

---
+++ src/app/core/models/avatarWithFrameComponent.types.ts
@@ -0,0 +1,3 @@
+export type shadowValues = "" | "l-shadow" | "s-shadow-white" | "s-shadow-green"
+export type frameValues = "" | "small" | "normal" | "medium" | "large";
+export type backgroundValues = "" | "secondary-dark-color";
