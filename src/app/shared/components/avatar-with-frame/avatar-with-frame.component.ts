968c3a9398a5cb1376fc4425b2ef1c769ac50a74
Alejandro25RA <alejito25raiders@gmail.com>
2024-05-02 11:32:22 -0500

    import CommonModule and types that component uses

--- src/app/shared/components/avatar-with-frame/avatar-with-frame.component.ts
+++ src/app/shared/components/avatar-with-frame/avatar-with-frame.component.ts
@@ -1,17 +1,22 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { AvatarService } from '../../services/avatar.service';
+import { backgroundValues, frameValues, shadowValues } from '../../../core/models/avatarWithFrameComponent.types';
+import { CommonModule } from '@angular/common';
 
 const defaultAvatarUrl = 'assets/images/unknown.webp';
 
 @Component({
   selector: 'app-avatar-with-frame',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './avatar-with-frame.component.html',
   styleUrl: './avatar-with-frame.component.css'
 })
 export class AvatarWithFrameComponent implements OnChanges {
   @Input() avatarId: number = 10;
+  @Input() shadow: shadowValues = "";
+  @Input() frameMesuare:frameValues = "";
+  @Input() background: backgroundValues = "";
 
   avatarUrl = defaultAvatarUrl; // Url by defect
 
