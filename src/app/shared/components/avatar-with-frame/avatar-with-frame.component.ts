65c8e142b2907925c9deb8f23705c1423f212263
Alejandro25RA <alejito25raiders@gmail.com>
2024-05-02 09:05:28 -0500

    Inject avatar.service and add the component logic
    add @Iput() avatarId
    add property avatarUrl

--- src/app/shared/components/avatar-with-frame/avatar-with-frame.component.ts
+++ src/app/shared/components/avatar-with-frame/avatar-with-frame.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { AvatarService } from '../../services/avatar.service';
+
+const defaultAvatarUrl = 'assets/images/unknown.webp';
 
 @Component({
   selector: 'app-avatar-with-frame',
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
   templateUrl: './avatar-with-frame.component.html',
   styleUrl: './avatar-with-frame.component.css'
 })
-export class AvatarWithFrameComponent {
+export class AvatarWithFrameComponent implements OnChanges {
+  @Input() avatarId: number = 10;
 
+  avatarUrl = defaultAvatarUrl; // Url by defect
+
+  constructor(private avatarService:AvatarService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['avatarId'] && !changes['avatarId'].firstChange) {
+      this.updateAvatarUrl();
+    }
+  }
+
+  updateAvatarUrl() {
+    if(this.avatarId !== undefined && this.avatarId >= 0){
+      this.avatarUrl = this.avatarService.getAvatarUrlById(this.avatarId);
+    } else {
+      this.avatarUrl = defaultAvatarUrl;
+    }
+  }
 }
