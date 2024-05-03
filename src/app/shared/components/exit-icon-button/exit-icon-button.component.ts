adccbaf352863d72043b4d868e5b5a89144e99e7
Alejandro25RA <alejito25raiders@gmail.com>
2024-05-02 22:20:08 -0500

    add component exit-icon-button

---
+++ src/app/shared/components/exit-icon-button/exit-icon-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exit-icon-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './exit-icon-button.component.html',
+  styleUrl: './exit-icon-button.component.css'
+})
+export class ExitIconButtonComponent {
+
+}
