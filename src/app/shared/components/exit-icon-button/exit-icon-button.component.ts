69390d08e43f15491b6ce361678cc72104d57b9e
Alejandro25RA <alejito25raiders@gmail.com>
2024-05-03 13:55:20 -0500

    add logic of component
    close modal
    open modal
    redirect to home page

--- src/app/shared/components/exit-icon-button/exit-icon-button.component.ts
+++ src/app/shared/components/exit-icon-button/exit-icon-button.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component';
+import { CustomButtonComponent } from '../custom-btn/custom-button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-exit-icon-button',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent,CustomButtonComponent],
   templateUrl: './exit-icon-button.component.html',
   styleUrl: './exit-icon-button.component.css'
 })
 export class ExitIconButtonComponent {
 
+  stateModal = false;
+
+  constructor (private router: Router){}
+  openModal() {
+    this.stateModal = true;
+    document.body.classList.add('without-overflow');
+  }
+
+  closeModal() {
+    this.stateModal = false;
+    document.body.classList.remove('without-overflow');
+  }
+
+  redirect() {
+    this.closeModal();
+    this.router.navigate(['/home']);
+  }
 }
