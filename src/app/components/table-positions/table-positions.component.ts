2fe107c5e591bfc9aecc5cb323f9c5ecf4805882
NicolasLLans <nicolaslautaro22@gmail.com>
2024-05-06 04:56:18 -0300

    feat: tablePosition load players

--- src/app/components/table-positions/table-positions.component.ts
+++ src/app/components/table-positions/table-positions.component.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
 import { PlayerResultsComponent } from '../player-results/player-results.component';
+import { PLAYERS_SELECTS } from '../../store/players/players.selectors';
+import { Store } from '@ngrx/store';
 
 // TODO consume and implements table information
 @Component({
   selector: 'app-table-positions',
   standalone: true,
-  imports: [
-    CommonModule,
-    PlayerResultsComponent
-  ],
+  imports: [CommonModule, PlayerResultsComponent],
   templateUrl: './table-positions.component.html',
   styleUrl: './table-positions.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TablePositionsComponent {
-  players = [1, 2, 3, 4, 5];
+export class TablePositionsComponent implements OnInit {
+  private store = inject(Store);
+  players$ = this.store.select(PLAYERS_SELECTS.selectPlayers);
+  players: number = 0;
+
+  ngOnInit(): void {
+    this.players$.subscribe(res => {
+      return (this.players = res.length);
+    });
+  }
 }
