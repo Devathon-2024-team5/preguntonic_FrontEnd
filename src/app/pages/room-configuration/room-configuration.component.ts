8244934d076eb0ac5d879207c14ed1d733925255
NicolasLLans <nicolaslautaro22@gmail.com>
2024-05-08 04:56:33 -0300

    feat:header in room-configuration and join-room

--- src/app/pages/room-configuration/room-configuration.component.ts
+++ src/app/pages/room-configuration/room-configuration.component.ts
@@ -4,14 +4,16 @@ import { HomeComponent } from '../home/home.component';
 import { Store } from '@ngrx/store';
 import { GAME_ACTIONS } from '../../store/game/game.actions';
 import { CustomButtonComponent } from '../../shared/components/custom-btn/custom-button.component';
+import { LogoTitleComponent } from "../../shared/components/logo-title/logo-title.component";
+import { ExitIconButtonComponent } from "../../shared/components/exit-icon-button/exit-icon-button.component";
 
 @Component({
-  selector: 'app-room-configuration',
-  standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [FormsModule, HomeComponent, CustomButtonComponent],
-  templateUrl: './room-configuration.component.html',
-  styleUrl: './room-configuration.component.css',
+    selector: 'app-room-configuration',
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: './room-configuration.component.html',
+    styleUrl: './room-configuration.component.css',
+    imports: [FormsModule, HomeComponent, CustomButtonComponent, LogoTitleComponent, ExitIconButtonComponent]
 })
 export class RoomConfigurationComponent {
   private readonly store = inject(Store);
