cabb4305bed360fb33753061110e2a26de8357c3
Alejandro25RA <alejito25raiders@gmail.com>
2024-05-16 20:09:01 -0500

    add property value in InputConfig.interface

--- src/app/shared/interfaces/InputConfig.interface.ts
+++ src/app/shared/interfaces/InputConfig.interface.ts
@@ -3,6 +3,7 @@ import { InputType } from "../types/input.type";
 export interface IInputConfig {
   placeholder: string;
   name: string;
+  value: string;
   type?: InputType;
   maxletngth?: number;
   classList?: Record<string, boolean>;
