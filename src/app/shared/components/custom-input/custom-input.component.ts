cc8f368d17b58039bf59d6964702c33d1bc76e12
Alejandro25RA <alejito25raiders@gmail.com>
2024-05-19 11:30:40 -0500

    feat: add css class change logics to apply in Comp

--- src/app/shared/components/custom-input/custom-input.component.ts
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { IInputConfig } from '../../interfaces/InputConfig.interface';
 import { NgClass } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -10,12 +10,27 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './custom-input.component.html',
   styleUrl: './custom-input.component.css'
 })
-export class CustomInputComponent {
+export class CustomInputComponent implements OnChanges{
   @Input() inputConfig: IInputConfig = {} as IInputConfig;
   @Output() valueChange: EventEmitter<string> = new EventEmitter<string>;
+  classes: string[] = [];
 
   onValueChange(event:any){
     this.inputConfig.value = event;
     this.valueChange.emit(this.inputConfig.value);
+  }
+
+  updateClassesToApply() {
+    this.classes = ['c-input'];
+    if(this.inputConfig.hasValidationError) {
+      this.classes.push('c-input--invalid')
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const hasValidationErrorChange = changes["inputConfig"].currentValue?.hasValidationError !== changes["inputConfig"].previousValue?.hasValidationError;
+    if(hasValidationErrorChange) {
+      this.updateClassesToApply();
+    }
   }
 }
